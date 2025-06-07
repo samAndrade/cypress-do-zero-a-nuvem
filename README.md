77875f4490876abc0c37a3460e712f1541bf11d0
Samir Andrade <samir.andrade26@gmail.com>
2025-06-07 20:37:22 -0300

    aprendizado inicial com Cypress - configuração, primeiros testes e documentação

--- README.md
+++ README.md
@@ -1,28 +1,33 @@
-# 🌲 Cypress, do Zero à Nuvem ☁️
+# 😎 Cypress, do Zero à Nuvem ☁️
 
-👋 Seja bem-vindo(a)!
+Projeto de exemplo para o curso **"Cypress, do Zero à Nuvem"** ou a escola online Talking About Testing.
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai amar esse curso. ❤️
+## Pré-requisitos
 
-## O que você vai aprender?
+É necessário ter o git, Node.js e npm instalados para clonar e executar este projeto.
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar upload de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
-- Como executar os testes em um _workflow_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como integrar seu _workflow_ de integração contínua com o Cypress Cloud (o serviço de gestão de testes do Cypress na nuvem)
+> Eu usei as versões `2.49.0`, `v22.14.0` e `10.9.2` do git, Node.js e npm, respectivamente. Sugiro que você use as mesmas ou versões LTS mais recentes.
 
-## Vamos começar?
+## Instalação
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+Execute `npm install` (ou `npm i` para a versão abreviada) para instalar as dependências de desenvolvimento.
 
-___
+## Testes
 
-Este é um curso da **Escola Talking About Testing**.
+Neste projeto, você pode executar os testes em visualização de **desktop** ou **mobile**.
+
+### Desktop
+
+Execute `npm test` (ou `npm t` para a versão abreviada) para executar os testes em modo headless em uma visualização de desktop.
+
+Ou, execute `npm run cy:open` para abrir o Cypress App em uma visualização de desktop.
+
+### Mobile
+
+Execute `npm run test:mobile` para executar os testes em modo headless em uma visualização mobile.
+
+Ou, execute `npm run cy:open:mobile` para abrir o Cypress App em uma visualização mobile.
+
+---
+
+Este projeto foi criado com 💙 por [Samir Andrade](https://www.linkedin.com/in/samirandrade26/).
---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,214 @@
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  })
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preencher os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Thiago')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('teste@gmai.com')
+    cy.get('#open-text-area').type('Apenas realizando testes na digitação do texto referente ao curso Cypress da Udemy!', { delay: 0 })
+    //cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success > strong').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Fernando')
+    cy.get('#lastName').type('Costa')
+    cy.get('#email').type('teste_gmail.com')
+    cy.get('#open-text-area').type('Apenas realizando testes na digitação do texto referente ao curso Cypress da Udemy!', { delay: 0 })
+    // cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+  it('campo telefone continua vazio quando preenchido com um valor não númerico', () => {
+    cy.get('#phone')
+      .type('abcde')
+      .should('have.value', '')
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Fernando')
+    cy.get('#lastName').type('Costa')
+    cy.get('#email').type('teste@gmail.com')
+    cy.get('#open-text-area').type('Curso Cypress Udemy!')
+    cy.get('#phone-checkbox').check()
+    cy.contains('Enviar').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('Fernando')
+      .should('have.value', 'Fernando')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Costa')
+      .should('have.value', 'Costa')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('teste@gmail.com')
+      .should('have.value', 'teste@gmail.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('92981924447')
+      .should('have.value', '92981924447')
+      .clear()
+      .should('have.value', '')
+    cy.get('#open-text-area')
+      .type('Curso Cypress Udemy!')
+      .should('have.value', 'Curso Cypress Udemy!')
+      .clear()
+      .should('have.value', '')
+    // cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    // cy.get('button[type="submit"]').click()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+  it('envia o formulário com sucesso usando um comando customizado', () => {
+    // const data = {
+    //   firstName: 'Gustavo',
+    //   lastName: 'Costa',
+    //   email: 'teste@gmail.com',
+    //   text: 'Curso Cypress'
+    // }
+
+    // cy.fillMandatoryFieldsAndSubmit(data)
+
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+
+  })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Leal')
+    cy.get('#email').type('teste@gmai.com')
+    cy.get('#open-text-area').type('Curso Udemy', { delay: 0 })
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor', () => {
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Leal')
+    cy.get('#email').type('teste@gmai.com')
+    cy.get('#open-text-area').type('Curso Udemy', { delay: 0 })
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Leal')
+    cy.get('#email').type('teste@gmai.com')
+    cy.get('#open-text-area').type('Curso Udemy', { delay: 0 })
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+
+    cy.contains('button', 'Enviar').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('marca o tipo de atendimento "Feedback', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('be.checked')
+  })
+
+  it('marca cada  tipo de atendimento', () => {
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
+  })
+
+  it('marca ambos os checkboxes, depois desmarca o último', () => {
+    cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should("not.be.checked")
+  })
+
+  it('seleciona um arquivo da pasta fixtures', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('seleciona um arquivo simulando um drag-and-drop', () => {
+    cy.get('#file-upload')
+      .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+    cy.fixture('example.json').as('exampleFile')
+    cy.get('#file-upload')
+      .selectFile('@exampleFile')
+      .should(input => {
+        expect(input[0].files[0].name).to.equal('example.json')
+      })
+  })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .should('have.attr', 'href', 'privacy.html')
+    .and('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .invoke('removeAttr', 'target')
+    .click()
+    
+    cy.contains('h1', 'CAC TAT - Política de Privacidade')
+    .should('be.visible')
+  })
+
+})
